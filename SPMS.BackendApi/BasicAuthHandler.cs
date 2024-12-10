33498d051f956774de16723b1ec1ede4cdb84bc1
theinhtay <theinhtay@aceplussolutions.com>
2024-12-10 13:48:17 +0630

    feat: Add basic authentication middle

---
+++ SPMS.BackendApi/BasicAuthHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace SPMS.BackendApi;
+
+public class BasicAuthHandler
+{
+    private readonly RequestDelegate next;
+
+    public BasicAuthHandler(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.ContainsKey("Authorization"))
+        {
+            //context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            ////context.Response.StatusCode = 401;
+            //await context.Response.WriteAsync("Unauthorized");
+            await RespondUnauthorized(context, "Missing Authorization Header");
+            return;
+        }
+
+        // Basic userid:password
+        var header = context.Request.Headers["Authorization"].ToString();
+        var encodeCreds = header.Substring(6);
+        var creds = Encoding.UTF8.GetString(Convert.FromBase64String(encodeCreds));
+        string[] uidpwd = creds.Split(':',2);
+
+        if (uidpwd.Length != 2)
+        {
+            await RespondUnauthorized(context, "Invalid Authorization Header Format");
+            return;
+        }
+
+        var uid = uidpwd[0];
+        var password = uidpwd[1];
+
+        if(uid != "admin" || password != "123456")
+        {
+            await RespondUnauthorized(context, "Invalid Username or Password");
+            return;
+        }
+        await next(context);
+    }
+
+    private static async Task RespondUnauthorized(HttpContext context, string message)
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        context.Response.Headers["WWW-Authenticate"] = "Basic realm=\"MyApp\"";
+        await context.Response.WriteAsync(message);
+    }
+}
--- SPMS.BackendApi/Program.cs
+++ SPMS.BackendApi/Program.cs
@@ -25,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<BasicAuthHandler>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
